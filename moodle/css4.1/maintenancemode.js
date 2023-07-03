1d64492118d6504c6bb9c32e4e5d2729c3d185cc
James Mackay <jamesrossmackay@gmail.com>
2023-07-04 07:35:20 +0800

    Update maintenancemode.js

--- moodle/css4.1/maintenancemode.js
+++ moodle/css4.1/maintenancemode.js
@@ -1,5 +1,5 @@
 maintenanceMessageWarning();
 
 function maintenanceMessageWarning() {
-    document.getElementById("page-header").insertAdjacentHTML("afterbegin", "<a href='https://help.eitidc.com/view-documentation/eit-system-maintenance' class='maintenanceStickyNotice' target='_blank'><strong>Maintenance Notice:</strong><br>Moodle will be undergoing scheduled maintenance on Tuesday at 11PM UTC for approximately 1 hour (until 12AM UTC). Please avoid using Moodle throughout this time.<br><u>Click here to read more</u></a>");
+    document.getElementById("page-header").insertAdjacentHTML("afterbegin", "<a href='https://help.eitidc.com/documentation/eit-system-maintenance' class='maintenanceStickyNotice' target='_blank'><strong>Maintenance Notice:</strong><br>Moodle will be undergoing scheduled maintenance on Tuesday at 11PM UTC for approximately 1 hour (until 12AM UTC). Please avoid using Moodle throughout this time.<br><u>Click here to read more</u></a>");
 }
